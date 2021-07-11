f22a30facfdf4b567f5dd43246645599fc32803f
Prcopt <28332530+prcopt@users.noreply.github.com>
2021-07-11 16:20:58 +0530

    Create inventory_cost function

--- Work/invcost.py
+++ Work/invcost.py
@@ -1,10 +1,18 @@
-with open("Data/inventory.csv") as FH:
-    headers = next(FH)
-    total = 0.0
-    for line in FH:
-        parts = line.strip().split(',')
-        quant = int(parts[1])
-        price = float(parts[2])
-        total += quant*price
-        
-    print("Total Cost",total)
+import csv
+
+def inventory_cost(filename):
+    with open(filename) as FH:
+        rows = csv.reader(FH)
+        headers = next(rows)
+        total = 0.0
+        for row in rows:
+            quant = int(row[1])
+            price = float(row[2])
+            total += quant*price
+            
+    return total
+            
+
+cost = inventory_cost("Data/inventory.csv")
+print("Total Cost",cost)
+
