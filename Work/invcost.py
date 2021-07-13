8e1814ae6f153341a5aa96544fc6379237efaaf4
Prcopt <28332530+prcopt@users.noreply.github.com>
2021-07-13 20:22:44 +0530

    Add files via upload
    Following modifications done:
    1. Interactively pick the file name.
    2.If file does not exisit, used try...exception to trap error condition and terminate without 'angry messages' of python.
    3.folder of data file, automatically found from current directory (.py file) so that folder name need not be given as input 
    4. Works fine with File name given as argument in Command Line.
    5. If FileName not given as argument, software interactively pick the file name.

--- Work/invcost.py
+++ Work/invcost.py
@@ -1,27 +1,37 @@
 import csv
 import sys
+import pathlib
 
 def inventory_cost(filename):
-    with open(filename) as FH:
-        rows = csv.reader(FH)
-        headers = next(rows)
-        total = 0.0
-        for row in rows:
-            try:
-                quant = int(row[1])
-                price = float(row[2])
-                total += quant*price
-            except ValueError as e:
-                print("Bad row",row)
-                print("Reason",e)
-                
+    try:
+        with open(filename) as FH:
+            rows = csv.reader(FH)
+            print("Rows:",rows)
+            headers = next(rows)
+            total = 0.0
+            for row in rows:
+                try:
+                    quant = int(row[1])
+                    price = float(row[2])
+                    total += quant*price
+                except ValueError as e:
+                    print("Bad row",row)
+                    print("Reason",e)
+    except FileNotFoundError:
+        print("File:",filename[filename.rfind("\\")+1:]," Not Found: Execution Terminated")
+        sys.exit("")
     return total
-            
+
+def get_folder_path_and_filename(fname):
+    path = str(pathlib.Path(__file__).parent.resolve())+'\\Data\\'
+    return path+fname
+
 if len(sys.argv) == 2:
-    filename = sys.argv[1]
+    filename = get_folder_path_and_filename(sys.argv[1])
 else:
-    filename = 'Data/inventory.csv'
-    
+    fname = str.upper(input("Enter Inventory File Name:"))
+    filename = get_folder_path_and_filename(fname)
+
 cost = inventory_cost(filename)
 print("Total Cost",cost)
 
