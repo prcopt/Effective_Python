3acb7044c8f412eabd61be11e422cbf32a96f21c
Prcopt <28332530+prcopt@users.noreply.github.com>
2021-07-15 10:39:45 +0530

    Add files via upload
    Added  read_prices function to read the current prices of inventory into a dictionary
    used inventory data and current price data to compute :
    i. Total Inventory Cost
    ii. Present Value of Inventory
    iii. Gain / Loss

--- Work/report.py
+++ Work/report.py
@@ -1,4 +1,4 @@
-"""  Read Inventory file modified from List of Tuples to List of Dictionaries
+"""  
 
 """
 import sys
@@ -12,8 +12,6 @@ def read_inventory(filename):
         with open(filename) as FH:
             rows = csv.reader(FH)
             headers = next(rows)
-#            keys = headers.split()
-            print(headers)
             for row in rows:
                 inventory.append({headers[0]:row[0],headers[1]:int(row[1]),headers[2]:float(row[2])})
             i += 1
@@ -22,15 +20,46 @@ def read_inventory(filename):
         sys.exit("")
     return inventory
             
-            
+def read_prices(filename):
+    try:
+        prices = dict()
+        with open(filename) as FH:
+            rows = csv.reader(FH)
+            for row in rows:
+                try:
+                    prices[row[0]] = float(row[1])
+                except ValueError as e:
+                    print("Bad row",row)
+                    print("Reason:",e)
+                except IndexError as m:
+                    print("Skipping blank line....")
+    except FileNotFoundError:
+        print("File:",filename, "Not Found: Execution Terminated.")
+        sys.exit("")
+    return prices
+ 
             
 # Main starts here
 if __name__ == "__main__":
-    # Reading inventory file and building List of Dictionaries
+    # Reading inventory file and building List of tuples
     inventory = read_inventory("Data\inventory.csv")
-    print(inventory)
-    inv_cost = sum(p["quant"]*p["price"] for p in inventory)
-    print("inv_cost:",inv_cost)
+    curr_inv_cost = sum(p["quant"]*p["price"] for p in inventory)
+    print("Total cost:",curr_inv_cost)
+    
+    # Reading latest price file and building alist of Dictionary
+    prices = read_prices("Data\prices.csv")
+    new_inv_cost = 0.0
+    
+    # Compute present value of inventory
+    for p in inventory:
+            if p['name'] in list(prices):
+                idx = list(prices).index(p['name'])
+                new_inv_cost += p['quant']*prices[p['name']]
+    print("Present Value:",new_inv_cost)
+    
+    # Printing Total Gain/Loss
+    print("Total Gain:",new_inv_cost - curr_inv_cost)
+
 
 
         
