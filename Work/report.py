b71c85669e8803815ce648d337613c78fadb8683
Prcopt <28332530+prcopt@users.noreply.github.com>
2021-07-15 16:38:20 +0530

    Add files via upload
    Formatted Reports added. Same report with and witout currency symbol. 
    code cleaned up.

--- Work/report.py
+++ Work/report.py
@@ -1,8 +1,7 @@
 """  
-
+WORKING WITH DATA: Inventory Report Generation
 """
 import sys
-import invcost as k
 import csv
 
 def read_inventory(filename):
@@ -37,8 +36,22 @@ def read_prices(filename):
         print("File:",filename, "Not Found: Execution Terminated.")
         sys.exit("")
     return prices
- 
-            
+
+""" make_report function build a table that contain product name, qty, Original price and change of price
+     input: product ( current inventory) and new price list.
+     output: table containg items as above.
+"""
+def make_report(inventory,prices):
+    new_price = list()
+    change = list()
+    for p in inventory:
+        if p['name'] in list(prices):
+            idx = list(prices).index(p['name'])
+            new_price.append(prices[p['name']])
+            change.append(prices[p['name']]- p['price'])
+    table = [(p["name"],p["quant"],np,c) for p,np,c in zip(inventory,new_price,change)]
+    return table
+
 # Main starts here
 if __name__ == "__main__":
     # Reading inventory file and building List of tuples
@@ -60,8 +73,23 @@ if __name__ == "__main__":
     # Printing Total Gain/Loss
     print("Total Gain:",new_inv_cost - curr_inv_cost)
 
+    # Making formatted Report
+    report = make_report(inventory,prices)
+    headers = ("Name","Quantity","Price","Change")
+    st = ""
+    for i in headers:
+        st += i.rjust(10)+" "
+    print(st)
+    print(f'{"":->10} {"":->10} {"":->10} {"":->10}')
 
-
+    for r in report:
+        print(f'{r[0]:>10s} {r[1]:>10d} {r[2]:>10.2f} {r[3]:>10.2f}')
+    print()
+    print(st)
+    print(f'{"":->10} {"":->10} {"":->10} {"":->10}')
+    for r in report:
+        price = '\u20B9'+ str(r[2])    
+        print(f'{r[0]:>10s} {r[1]:>10d} {price:>10s} {r[3]:>10.2f}')    
         
         
         
