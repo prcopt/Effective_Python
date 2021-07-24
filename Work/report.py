a9053f7b9886d850c4392278dbaa9cb69cec64e0
Prcopt <28332530+prcopt@users.noreply.github.com>
2021-07-24 14:49:42 +0530

    Add files via upload
    Q12 - Modified report.py to introduce header in read prices. inventory data  reading from inventorydate.csv

--- Work/report.py
+++ Work/report.py
@@ -1,32 +1,46 @@
 """  
-WORKING WITH DATA: Inventory Report Generation
+WORKING WITH DATA: Inventory Report Generation - Q12
 """
 import sys
 import csv
-
+import pathlib
 def read_inventory(filename):
     try:
         inventory = list()
-        i = 0
         with open(filename) as FH:
             rows = csv.reader(FH)
             headers = next(rows)
-            for row in rows:
-                inventory.append({headers[0]:row[0],headers[1]:int(row[1]),headers[2]:float(row[2])})
-            i += 1
+            for row_no,row in enumerate(rows,start=1):
+                try:
+                    values = row
+                    inventory.append(dict(zip(headers,values)))
+                except ValueError as e:
+                    print("ValueError: Row:",row_no,"Couldn't convert:",row)
+                    print("Reason:",e)
+                except IndexError as e:
+                    print("IndexError: Row:",row_no,"Skipping a row:",row)
+                    print("Reason:",e)
+                    continue
+                except KeyError as e:
+                    print("KeyError: Row:",row_no,"Skipping a row:",row)
+                    print("Reason:",e)
+  
     except FileNotFoundError:
         print("File:",filename, "Not Found: Execution Terminated.")
         sys.exit("")
     return inventory
             
 def read_prices(filename):
-    try:
+    try:        
         prices = dict()
-        with open(filename) as FH:
-            rows = csv.reader(FH)
+        with open(filename) as FHR:
+            rows = csv.reader(FHR)
+            header = ['name','price']
             for row in rows:
                 try:
-                    prices[row[0]] = float(row[1])
+                    values = row
+                    set = dict(zip(header,values))
+                    prices[set['name']] = float(set['price'])
                 except ValueError as e:
                     print("Bad row",row)
                     print("Reason:",e)
@@ -48,26 +62,26 @@ def make_report(inventory,prices):
         if p['name'] in list(prices):
             idx = list(prices).index(p['name'])
             new_price.append(prices[p['name']])
-            change.append(prices[p['name']]- p['price'])
+            change.append(float(prices[p['name']])- float( p['price']))
     table = [(p["name"],p["quant"],np,c) for p,np,c in zip(inventory,new_price,change)]
     return table
 
 # Main starts here
 if __name__ == "__main__":
     # Reading inventory file and building List of tuples
-    inventory = read_inventory("Data\inventory.csv")
-    curr_inv_cost = sum(p["quant"]*p["price"] for p in inventory)
+    inventory = read_inventory("Data\inventorydate.csv")
+    curr_inv_cost = sum(int(p["quant"])* float(p["price"]) for p in inventory)
     print("Total cost:",curr_inv_cost)
-    
+
     # Reading latest price file and building alist of Dictionary
-    prices = read_prices("Data\prices.csv")
+    prices = read_prices("Data\\prices.csv")
     new_inv_cost = 0.0
     
     # Compute present value of inventory
     for p in inventory:
             if p['name'] in list(prices):
                 idx = list(prices).index(p['name'])
-                new_inv_cost += p['quant']*prices[p['name']]
+                new_inv_cost += int(p['quant'])* float(prices[p['name']])
     print("Present Value:",new_inv_cost)
     
     # Printing Total Gain/Loss
@@ -83,13 +97,13 @@ if __name__ == "__main__":
     print(f'{"":->10} {"":->10} {"":->10} {"":->10}')
 
     for r in report:
-        print(f'{r[0]:>10s} {r[1]:>10d} {r[2]:>10.2f} {r[3]:>10.2f}')
+        print(f'{r[0]:>10s} {int(r[1]):>10d} {r[2]:>10.2f} {r[3]:>10.2f}')
     print()
     print(st)
     print(f'{"":->10} {"":->10} {"":->10} {"":->10}')
     for r in report:
         price = '\u20B9'+ str(r[2])    
-        print(f'{r[0]:>10s} {r[1]:>10d} {price:>10s} {r[3]:>10.2f}')    
+        print(f'{r[0]:>10s} {int(r[1]):>10d} {price:>10s} {r[3]:>10.2f}')    
         
         
         
